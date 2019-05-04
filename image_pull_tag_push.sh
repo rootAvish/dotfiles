1c8863183d78125ece19102bd9726ce4592eb61c
Avishkar Gupta <rootavish@gmail.com>
2019-05-04 19:59:22 +0530

    Fix reference to docker image
    Signed-off-by: Avishkar Gupta <rootavish@gmail.com>

--- image_pull_tag_push.sh
+++ image_pull_tag_push.sh
@@ -9,9 +9,9 @@ echo "Pulling from CI namespace " $1
 ## now loop through the above array
 for i in "${arr[@]}"
 do
-   echo docker pull registry.svc.ci.openshift.org/$1/stable/$i
-   echo docker tag registry.svc.ci.openshift.org/$1/stable/$i docker.io/avgupta/payload-components:$i
-   echo docker push docker.io/avgupta/payload-components:$i
+   docker pull registry.svc.ci.openshift.org/$1/stable:$i
+   docker tag registry.svc.ci.openshift.org/$1/stable:$i docker.io/avgupta/payload-components:$i
+   docker push docker.io/avgupta/payload-components:$i
    # or do whatever with individual element of the array
 done
 
